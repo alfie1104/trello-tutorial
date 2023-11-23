cc32823be318989602a0e431845678716a2b6d7e
MingiChae <alfie1104@gmail.com>
2023-11-23 12:44:20 +0900

    creating organization page

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,9 @@
+import { OrganizationSwitcher, auth } from "@clerk/nextjs";
+
+const OrganizationIdPage = () => {
+  const { userId, orgId } = auth();
+
+  return <div></div>;
+};
+
+export default OrganizationIdPage;
