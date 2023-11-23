0dbdd6db487706cb1134fc99b34b37da919310e1
MingiChae <alfie1104@gmail.com>
2023-11-23 13:58:09 +0900

    feat : setting organization pages
    - setting up organization pages
    - add Button variants (primary)

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,5 @@
-import { OrganizationSwitcher, auth } from "@clerk/nextjs";
-
 const OrganizationIdPage = () => {
-  const { userId, orgId } = auth();
-
-  return <div></div>;
+  return <div>Organization Page</div>;
 };
 
 export default OrganizationIdPage;
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,34 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: ["/"],
+  afterAuth(auth, req) {
+    if (auth.userId && auth.isPublicRoute) {
+      let path = "/select-org";
+
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
+
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+
+    if (!auth.userId && !auth.isPublicRoute) {
+      //public route가 아닌곳에 로그인되지 않은 사용자가 접근한 경우
+      //signIn으로 redirect시키고, 어느곳에 방문하려 했는지(req.url)를 함께 전송함
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  },
 });
 
 export const config = {
