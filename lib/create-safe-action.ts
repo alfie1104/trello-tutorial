5efb9575d271915952fe8256f6a48cde85a0cb22
peter_alfie <alfie1104@gmail.com>
2023-12-01 08:46:04 +0900

    Add comment on create-safe-action function

--- lib/create-safe-action.ts
+++ lib/create-safe-action.ts
@@ -10,6 +10,8 @@ export type ActionState<TInput, TOutput> = {
   data?: TOutput; //성공시 클라이언트로 전달될 데이터
 };
 
+//입력 매개변수 데이터의 유효성을 검사하고, 유효성 검사가 성공하면 제공된 핸들러를 호출하여 결과를 반환하는 '안전한 액션 함수'를 생성하는 것이 목적
+//코드의 안전성을 높이고 에러를 미리 방지가능
 export const createSafeAction = <TInput, TOutput>(
   schema: z.Schema<TInput>,
   handler: (validatedData: TInput) => Promise<ActionState<TInput, TOutput>>
