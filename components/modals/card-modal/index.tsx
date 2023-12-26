74c3aa1c7d07c8df674241e1f44ed808ce7b216d
peter_alfie <alfie1104@gmail.com>
2023-12-26 09:35:26 +0900

    feat : add card api
    - add card api
    - use card api at card-modal
    - add header component for card-modal

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -1,7 +1,11 @@
 "use client";
+import { useQuery } from "@tanstack/react-query";
 
 import { useCardModal } from "@/hooks/use-card-modal";
+import { CardWithList } from "@/types";
+import { fetcher } from "@/lib/fetcher";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { Header } from "./header";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -9,9 +13,16 @@ export const CardModal = () => {
   const onOpen = useCardModal((state) => state.onOpen);
   const onClose = useCardModal((state) => state.onClose);
 
+  const { data: cardData } = useQuery<CardWithList>({
+    queryKey: ["card", id],
+    queryFn: () => fetcher(`/api/cards/${id}`),
+  });
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent>I am a modal</DialogContent>
+      <DialogContent>
+        {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+      </DialogContent>
     </Dialog>
   );
 };
