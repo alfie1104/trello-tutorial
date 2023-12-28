2f59c64242ab515f9f98b1db80d6f8c11907e1bc
peter_alfie <alfie1104@gmail.com>
2023-12-29 08:01:46 +0900

    feat : add auditLog apis
    - add create-audit-log library to be used at actions
    - add router for fetching logs on a card

--- actions/create-card/index.ts
+++ actions/create-card/index.ts
@@ -4,10 +4,12 @@ import { auth } from "@clerk/nextjs";
 
 import { db } from "@/lib/db";
 import { createSafeAction } from "@/lib/create-safe-action";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 import { CreateCard } from "./schema";
 import { InputType, ReturnType } from "./types";
 import { revalidatePath } from "next/cache";
+import { createAuditLog } from "@/lib/create-audit-log";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -51,6 +53,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         listId,
         order: newOrder,
       },
+    });
+
+    await createAuditLog({
+      entityId: card.id,
+      entityTitle: card.title,
+      entityType: ENTITY_TYPE.CARD,
+      action: ACTION.CREATE,
     });
   } catch (error) {
     return {
